cf08240333c69fb930d3398d1320638e98b82afb
karla <110636796+KPAM25@users.noreply.github.com>
2025-01-26 16:15:06 -0600

    Create

---
+++ src/main/java/com/alomora/emd2025/EMD2025.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.alomora.emd2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author kpam2
+ */
+
+@SpringBootApplication
+public class EMD2025 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EMD2025.class, args); 
+    }
+}
